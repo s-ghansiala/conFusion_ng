38d139fdcf62265aaeec024e2fc3defc7696cb8e
Ghansiala <sandeep.ghansiala852@gmail.com>
2020-04-25 02:29:03 +0530

    Single Page Applications Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 
+
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
 
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { Dish } from '../shared/dish'
+
+import { DishService } from '../services/dish.service';
+
+
 
 
 @Component({
@@ -9,12 +16,19 @@ import { Dish } from '../shared/dish'
 })
 export class DishdetailComponent implements OnInit {
 
-  @Input()
   dish: Dish;
 
-  constructor() { }
+  constructor(private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    let id = this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
